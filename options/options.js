e7c2ebc6865483c11317f15ee364e19ad48d6bb1
Crowdbotics <team@crowdbotics.com>
2021-10-09 02:26:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "crimson_leaf_31254",
+  url: "https://crimson_leaf_31254.botics.co",
+  api: "https://crimson_leaf_31254.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
